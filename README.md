c1a06b0a8347f6a1b9e8c54537262c46b9a79d3f
mars1448 <lekeeseschool@gmail.com>
2022-07-20 12:29:39 -0700

    Updated functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sketch
+
 Etch-a-sketch
+
+This is a etch-a-sketch project aimed at creating an web app similar to etch-a-sketch using html, css, and javascript.
--- app.js
+++ app.js
@@ -10,9 +10,14 @@ const rainbowBtn = document.getElementById("rainbow");
 const colorBtn = document.getElementById("color");
 const clearBtn = document.getElementById("clear");
 
+// DECLARATIONS //
 const size = slider.value;
-let normal = "color";
-let defaultColor = color.value;
+const defaultColor = color.value;
+const defaultId = 'color'
+
+let currentColor = defaultColor;
+let currentSize = size;
+let currentId = defaultId;
 
 // FUNCTION TO RENDER THE GRID
 const makeRows = (rows) => {
@@ -22,12 +27,30 @@ const makeRows = (rows) => {
   for (let i = 0; i < rows * rows; i++) {
     const div = document.createElement("div");
     div.addEventListener("click", (e) => {
-      e.target.style.backgroundColor = changeColor(e);
+      e.target.style.backgroundColor = currentColor;
     });
     container.appendChild(div).classList.add("grid-item");
   }
 };
 
+function setColor(e) {
+  currentColor = e;
+  console.log(currentColor)
+}
+
+function reloadGrid() {
+  clear()
+  makeRows(currentSize);
+}
+
+function setSize(newSize) {
+  currentSize = newSize
+  return currentSize;
+}
+
+function clear() {
+  container.innerHTML = '';
+}
 // GENERATES A RANDOM COLOR
 const random = () => {
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -35,22 +58,24 @@ const random = () => {
   return `#${randomColor}`;
 };
 
+color.oninput = (e) => setColor(e.target.value)
+colorBtn.onclick = (e) => updateBtn(e)
+rainbowBtn.onclick = (e) => updateBtn(e)
+clearBtn.onclick = () => clear()
+
 let active = false;
 document.body.onmousedown = () => (active = true);
 document.body.onmouseup = () => (active = false);
 
-const updateBtn = (e) => {
-  normal = e;
-  console.log(normal);
-  return normal;
+function setBtn(e) {
+  activeBtn(e)
+  normal = e
 };
 
-const changeColor = (e) => {
-  console.log(normal);
-  if (e.type === "mouseover" && !active) return;
-  if ((normal = "color")) e.target.style.backgroundColor = defaultColor;
-  else if ((normal = "rainbow")) e.target.style.backgroundColor = random();
-};
+function setBtn(e) {
+
+}
+
 // color.value
 // rainbowBtn.addEventListener('click', () => {
 // div.addEventListener("click", (e) => {
@@ -62,13 +87,33 @@ const changeColor = (e) => {
 //   // active[e.target].style.backgroundColor = 'orange';
 // });
 
-const activeBtn = () => {
-  if ((normal = "color")) console.log("to");
-  else if ((normal = "rainbow")) console.log("ti");
-  else if ((normal = "clear")) console.log("ra");
-};
 
 window.onload = () => {
   makeRows(size);
   h1.textContent = `${slider.value} x ${slider.value}`;
 };
+
+// if ((normal = "color")) e.target.style.backgroundColor = defaultColor;
+//   else if ((normal = "rainbow")) e.target.style.backgroundColor = random();
+
+// const changeColor = (e) => {
+//   console.log(e.target.id)
+//   normal = e.target.id
+//   if (e.type === "mouseover" && !active) return;
+//   switch(e.target.id) {
+//     case 'color':
+//       normal = e.target.id
+//       e.target.style.backgroundColor = defaultColor;
+//       console.log('do')
+//       return
+//     case 'rainbow':
+//       normal = e.target.id
+//       console.log('doo')
+//       return
+//     case 'clear':
+//       normal = e.target.id
+//       console.log('dooo')
+//       return
+//   }
+//   return normal
+// };
